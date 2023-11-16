7bbb1e4460fba4717adf647933a319ec5ba705f4
lbrenig <lbrenig@uni-bremen.de>
2023-11-16 18:33:12 +0100

    fix transform usage in dataset

--- scripts/load_geoguessr_data.py
+++ scripts/load_geoguessr_data.py
@@ -48,24 +48,27 @@ def load_data(DATA_PATH: str, min_img: int = 0, size_constraints: bool = False, 
 
 
 class ImageDataset_from_df(Dataset):
-    def __init__(self, df,transform=None, target_transform=None):
-
+    def __init__(self, df,transform=None, target_transform=None, name='default_data'):
+        if self.target_transform:
+            self.captions = clip.tokenize(df["label"].apply(self.target_transform).tolist())
+        else:
+            self.captions = clip.tokenize(df["label"].tolist())
         self.images = df["path"].tolist()
-        self.captions = clip.tokenize(df["label"].tolist())
+        
         device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model, self.preprocessor = clip.load("ViT-B/32", device=device)
         self.transform = transform
         self.target_transform = target_transform
+        self.name = name
 
     def __len__(self):
         return len(self.captions)
 
     def __getitem__(self, idx):
-        
-        image = self.preprocessor(PIL.Image.open(self.images[idx])) #preprocess from clip.load
-        caption = self.captions[idx]
+        image = PIL.Image.open(self.images[idx])
         if self.transform:
             image = self.transform(image)
-        if self.target_transform:
-            caption = self.target_transform(caption)
+        image = self.preprocessor(image) #preprocess from clip.load
+        caption = self.captions[idx]
+
         return image,caption
