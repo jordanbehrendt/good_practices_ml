b499d4c341dc84cd73e6ded3e9a7bf87164f3edc
jordanbehrendt <jobe@uni-bremen.de>
2023-12-06 13:23:17 +0100

    restructured evaluate script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -12,56 +12,87 @@ import random
 import numpy as np
 from deepsig import aso
 
+def box_plot(df: pd.DataFrame, metric: str, experiment_name: str, experiment_dir: str):
+    sns.boxplot(data=df, showmeans=True)
+    plt.title(f"Boxplot of {metric} for experiment {experiment_name}")
+    plt.xlabel("Model")
+    plt.ylabel(f"{metric}")
+    plt.savefig(os.path.join(experiment_dir, f"{experiment_name}_{metric}_boxplot.png"))
+    plt.close()
+
+def violin_plot(df: pd.DataFrame, metric: str, experiment_name: str, experiment_dir: str):
+    sns.violinplot(data = df)
+    plt.title(f"Violinplot of {metric} for experiment {experiment_name}")
+    plt.xlabel("Model")
+    plt.ylabel(f"{metric}")
+    plt.savefig(os.path.join(experiment_dir, f"{experiment_name}_{metric}_violinplot.png"))
+    plt.close()
+
+def ttest(accs, metric: str, seed: int):
+    p_values = np.zeros((3,3))
+    num_datasets = len(accs)
+    for i in range(num_datasets):
+        for j in range(num_datasets):
+            p_values[i][j] = stats.ttest_ind(accs[i], accs[j], random_state=seed).pvalue
+    print(f'{metric} {p_values}')
+
+def aso_test(accs, metric: str, seed: int):
+    min_eps_values = np.zeros((3,3))
+    num_datasets = len(accs)
+    for i in range(num_datasets):
+        for j in range(num_datasets):
+            min_eps_values[i][j] = aso(accs[i], accs[j], seed=seed)
+    print(f'{metric} {min_eps_values}')
+
 def evaluate(REPO_PATH: str, experiment_name: str):
     seed = 1234
 
+    unsd_regions = pd.read_csv(os.path.join(REPO_PATH, 'data', 'UNSD_Methodology.csv'))[['Intermediate Region Name','Country or Area']]
     experiment_dir = os.path.join(REPO_PATH, "Experiments", experiment_name)
     experiment_accuracies = []
+    experiment_region_accuracies = []
     dataset_names = []
     for dir in os.listdir(experiment_dir):
 
         dataset_dir = os.path.join(experiment_dir, dir)
         if os.path.isdir(dataset_dir):
             dataset_accuracies = []
+            dataset_region_accuracies = []
             dataset_names.append(dir)
             for batch_file in os.listdir(dataset_dir):
-                batch_df = pd.read_csv(os.path.join(dataset_dir, batch_file))
-                dataset_accuracies.append(metrics.accuracy_score(batch_df["label"].to_list(), batch_df["predicted labels"].to_list()))
+                if not os.path.isdir(os.path.join(dataset_dir, batch_file)):
+                    batch_df = pd.read_csv(os.path.join(dataset_dir, batch_file))
+                    if dir == 'street_prompt':
+                        batch_df['Predicted labels'] = batch_df['Predicted labels'].str.replace('A google streetview image from This image shows the country ', '')
+                    elif dir == 'elab_prompt':
+                        batch_df['Predicted labels'] = batch_df['Predicted labels'].str.replace('This image shows the country ', '')
+                    batch_df = pd.merge(batch_df, unsd_regions, left_on='Predicted labels', right_on='Country or Area', how='inner')
+                    batch_df = batch_df.rename(columns={'Intermediate Region Name': 'predicted_region'})
+                    del batch_df['Country or Area']
+                    batch_df = pd.merge(batch_df, unsd_regions, left_on='label', right_on='Country or Area', how='inner')
+                    batch_df = batch_df.rename(columns={'Intermediate Region Name': 'reference_region'})
+                    del batch_df['Country or Area']
+                    dataset_accuracies.append(metrics.accuracy_score(batch_df["label"].to_list(), batch_df["Predicted labels"].to_list()))
+                    dataset_region_accuracies.append(metrics.accuracy_score(batch_df["reference_region"].to_list(), batch_df["predicted_region"].to_list()))
             experiment_accuracies.append(dataset_accuracies)
+            experiment_region_accuracies.append(dataset_region_accuracies)
     
     exp_dict = {}
     for dataset_name, exp_acc in zip(dataset_names, experiment_accuracies):
         exp_dict[dataset_name] = exp_acc
-
     exp_df = pd.DataFrame(exp_dict)
-    sns.boxplot(data=exp_df, showmeans=True)
-    plt.title(f"Boxplot of Accuracies for experiment {experiment_name}")
-    plt.xlabel("Model")
-    plt.ylabel("Accuracy")
-    plt.savefig(os.path.join(experiment_dir, f"{experiment_name}_boxplot.png"))
 
-    plt.close()
 
-    sns.violinplot(data = exp_df)
-    plt.title(f"Violinplot of Accuracies for experiment {experiment_name}")
-    plt.xlabel("Model")
-    plt.ylabel("Accuracy")
-    plt.savefig(os.path.join(experiment_dir, f"{experiment_name}_violinplot.png"))
 
-    p_values = np.zeros((3,3))
-    num_datasets = len(experiment_accuracies)
-    for i in range(num_datasets):
-        for j in range(num_datasets):
-            p_values[i][j] = stats.ttest_ind(experiment_accuracies[i], experiment_accuracies[j], random_state=seed).pvalue
-
-    print(p_values)
-
-    min_eps_values = np.zeros((3,3))
-    for i in range(num_datasets):
-        for j in range(num_datasets):
-            min_eps_values[i][j] = aso(experiment_accuracies[i], experiment_accuracies[j], seed=seed)
-
-    print(min_eps_values)
+    exp_region_dict = {}
+    for dataset_name, exp_region_acc in zip(dataset_names, experiment_region_accuracies):
+        exp_region_dict[dataset_name] = exp_region_acc
+    exp_region_df = pd.DataFrame(exp_region_dict)
+    region_metric = 'Region Accuracy'
+    box_plot(exp_region_df, region_metric, experiment_name, experiment_dir)
+    violin_plot(exp_region_df, region_metric, experiment_name, experiment_dir)
+    ttest(experiment_region_accuracies, region_metric, seed)
+    aso_test(experiment_region_accuracies, region_metric, seed)
 
     def model_performance(probs: List[float], labels: List[str], possible_captions: List):
         """Saves the conifdence of the best prediction with the predicted an correct label on a batch.
@@ -84,8 +115,6 @@ def evaluate(REPO_PATH: str, experiment_name: str):
             'label' : labels
         })
         return performance_data
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Pretrained Model')
