4979e281e4fc09d1f281add9a7a0de3c14504b7d
lbrenig <lbrenig@uni-bremen.de>
2023-12-06 12:38:48 +0100

    promptcompare experiment with modelTester

--- models/promt_compar.py
+++ models/promt_compar.py
@@ -3,21 +3,17 @@ sys.path.append('.')
 #----------------------------------------------
 import torch
 import clip
-import tqdm
 import yaml
 import argparse
 import os
 import scripts.load_geoguessr_data as geo_data
-import scripts.helpers as scripts
-import numpy as np
 import pandas as pd
-from torch.utils.data import DataLoader
+import model_tester as testing
 import random
 import sklearn.model_selection
-from models.test_model import test_model
 
 
-def zero_shot_prediction(DATA_PATH: str, debug: bool, config_path: str):
+def run_experiment(DATA_PATH: str, debug: bool, config_path: str):
     with open(config_path) as file:
         config = yaml.safe_load(file)
         min_img = config['min_img']
@@ -34,7 +30,7 @@ def zero_shot_prediction(DATA_PATH: str, debug: bool, config_path: str):
     geoguessr_df = geo_data.load_data(DATA_PATH=DATA_PATH, min_img = min_img, max_img = max_img, size_constraints= True, debug_data=debug)
     
     # create country list for the clip model to use as possible labels
-    labels = geoguessr_df['label'].unique()
+    country_list = pd.read_csv(os.path.join(REPO_PATH,"data_exploration","image_distribution.csv"))['label'].to_list()
 
     if debug:
         test = geoguessr_df
@@ -42,12 +38,19 @@ def zero_shot_prediction(DATA_PATH: str, debug: bool, config_path: str):
     else:  
         train, test = sklearn.model_selection.train_test_split(geoguessr_df, test_size = test_size, random_state = seed, stratify = geoguessr_df["label"], shuffle=True)
 
-    standard_dataset = geo_data.ImageDataset_from_df(test,transform=preprocessor)
-    v1_dataset = geo_data.ImageDataset_from_df(test, transform=preprocessor, target_transform=(lambda x : f"This image shows the country {x}"), name="elab_prompt")
-    v2_dataset = geo_data.ImageDataset_from_df(test, transform=preprocessor,target_transform=(lambda x : f"A google streetview image from {x}"), name="street_prompt")
+    test_dataset = geo_data.ImageDataset_from_df(test,transform=preprocessor)
+    # v1_dataset = geo_data.ImageDataset_from_df(test, transform=preprocessor, target_transform=(lambda x : f"This image shows the country {x}"), name="elab_prompt")
+    # v2_dataset = geo_data.ImageDataset_from_df(test, transform=preprocessor,target_transform=(lambda x : f"A google streetview image from {x}"), name="street_prompt")
 
-    dataset_collection = [standard_dataset,v1_dataset,v2_dataset]
-    test_model(DATA_PATH,REPO_PATH,model,'pretrained','prompt_compare',dataset_collection,labels,batch_size,seed)
+    #dataset_collection = [standard_dataset,v1_dataset,v2_dataset]
+    #test_model(DATA_PATH,REPO_PATH,model,'pretrained','prompt_compare',dataset_collection,labels,batch_size,seed)
+    standard_tester = testing.ModelTester(test_dataset, model, (lambda x : x), batch_size, country_list, seed, REPO_PATH, 'pretrained','no_prompt','experiment0')
+    standard_tester.run_test()
+    elab_tester = testing.ModelTester(test_dataset, model, (lambda x : f"This image shows the country {x}"), batch_size, country_list, seed, REPO_PATH, 'pretrained','elab_prompt','experiment0')
+    elab_tester.run_test()
+    streetveiw_tester = testing.ModelTester(test_dataset, model,(lambda x : f"A google streetview image from {x}"), batch_size, country_list, seed, REPO_PATH, 'pretrained','streetveiw_prompt','experiment0')
+    streetveiw_tester.run_test()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Pretrained Model')
@@ -61,4 +64,4 @@ if __name__ == "__main__":
         paths = yaml.safe_load(file)
         DATA_PATH = paths['data_path'][args.user]
         REPO_PATH = paths['repo_path'][args.user]
-        zero_shot_prediction(DATA_PATH,args.debug,args.config_path)
+        run_experiment(DATA_PATH,args.debug,args.config_path)
