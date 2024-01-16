0e308e45df03dcf8e57ff93be122b82c616b669f
Leon <lbrenig@uni-bremen.de>
2024-01-16 23:21:57 +0100

    fix bugs

--- models/model_trainer.py
+++ models/model_trainer.py
@@ -23,7 +23,7 @@ class ModelTrainer():
         self.learning_rate = learning_rate
         self.optimizer = optim.Adam(model.parameters(), lr=learning_rate)
         self.country_list = pd.read_csv(country_list)
-        self.region_list = pd.read_csv(region_list)
+        self.region_list = pd.read_csv(region_list,delimiter=';')
         self.criterion = Regional_Loss(self.country_list, self.region_list)
         self.writer = SummaryWriter()
         self.start_training()
@@ -39,7 +39,7 @@ class ModelTrainer():
             float: Average loss for the epoch
         """
         running_loss = 0.
-        last_loss = []
+        last_loss = 0.
 
         # Here, we use enumerate(training_loader) instead of
         # iter(training_loader) so that we can track the batch
@@ -59,9 +59,9 @@ class ModelTrainer():
             # Gather data and report
             running_loss += loss.item()
             if i % 50 == 49:
-                last_loss.append(running_loss / 50) # loss per batch
+                last_loss= (running_loss / 50) # loss per batch
                 print('  batch {} loss: {}'.format(i + 1, last_loss))
-                tb_x = epoch_index * len(self.training_loader) + i + 1
+                tb_x = epoch_index * len(self.train_loader) + i + 1
                 self.writer.add_scalar('Loss/train', last_loss, tb_x)
                 running_loss = 0.
 
@@ -107,24 +107,43 @@ class Regional_Loss(torch.nn.Module):
         loss = []
         for output, target in zip(outputs,targets):
             ref_country = target
-            ref_alph2code = self.country_list[self.country_list['Country'] == target]['Alpha2Code'].values[0]
-            ref_region = self.region_list[self.region_list['ISO-alpha2 Code'] == ref_alph2code]['Intermediate Region Name'].values[0]
-            pred_country = self.country_list['Country'].iloc[np.argmax(output.detach().numpy())]
-            pred_alpha2code = self.country_list[self.country_list['Country'] == pred_country]['Alpha2Code'].values[0]
-            pred_region = self.region_list[self.region_list['ISO-alpha2 Code'] == pred_alpha2code]['Intermediate Region Name'].values[0]
+            ref_country_row = self.country_list[self.country_list['Country'] == ref_country]
+            if ref_country_row.empty:
+                print(f"Country {ref_country} not found in country list")
+                continue
+            ref_alph2code = ref_country_row['Alpha2Code'].values[0]
+            ref_region_row = self.region_list[self.region_list['ISO-alpha2 Code'] == ref_alph2code]
+            if ref_region_row.empty:
+                print(f"Alpha2Code {ref_alph2code} not found in region list")
+                continue
+            ref_region = ref_region_row['Intermediate Region Name'].values[0]
+            pred_country = self.country_list['Country'].iloc[torch.argmax(output).item()]
+            pred_country_row = self.country_list[self.country_list['Country'] == pred_country]
+            if pred_country_row.empty:
+                print(f"Country {pred_country} not found in country list")
+                continue
+            pred_alpha2code = pred_country_row['Alpha2Code'].values[0]
+            pred_region_row = self.region_list[self.region_list['ISO-alpha2 Code'] == pred_alpha2code]
+            if pred_region_row.empty:
+                print(f"Alpha2Code {pred_alpha2code} not found in region list")
+                continue
+            pred_region = pred_region_row['Intermediate Region Name'].values[0]
 
             if ref_country == pred_country:
-                loss.append(0)
+                loss.append(0.)
             elif ref_region == pred_region:
                 loss.append(0.5)
             else:
-                loss.append(1)
-        return np.array(loss).mean()
+                loss.append(1.)
+        loss = torch.tensor(loss)
+        loss.requires_grad = True
+
+        return loss.mean()
 
 # Directory containing CSV files
-directory = '/share/temp/bjordan/good_practices_in_machine_learning/good_practices_ml/Embeddings/Image/'
-country_list = "/share/temp/bjordan/good_practices_in_machine_learning/good_practices_ml/data_finding/country_list.csv"
-region_list = "/share/temp/bjordan/good_practices_in_machine_learning/good_practices_ml/data/UNSD_Methodology.csv"
+directory = '/home/lbrenig/Dokumente/GPML/good_practices_ml/Embeddings/Image'
+country_list = "/home/lbrenig/Dokumente/GPML/good_practices_ml/data_finding/country_list.csv"
+region_list = "/home/lbrenig/Dokumente/GPML/good_practices_ml/data/UNSD_Methodology.csv"
 
 
 # Get a list of filenames that start with "geoguessr" and end with ".csv"
