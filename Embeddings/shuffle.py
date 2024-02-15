45d833055f115f6a55e31b9b22ea7ee8bd54c9e9
KieranProlog <kieran@prolog.work>
2024-02-15 08:35:31 +0100

    shuffle strongly balanced training sets

--- Embeddings/shuffle.py
+++ Embeddings/shuffle.py
@@ -16,53 +16,89 @@ def save_batches(path, name, dataframe):
         # Save or process the batch as needed
         batch_df.to_csv(f"{path}/{name}_{i}.csv", index=False)
 
-balanced_directory = './Embeddings/Training/Balanced'
-balanced_file_list = [file for file in os.listdir(balanced_directory)]
+# balanced_directory = './Embeddings/Training/Balanced'
+# balanced_file_list = [file for file in os.listdir(balanced_directory)]
+
+# # Initialize an empty list to store DataFrames
+# balanced_dfs = []
+
+# # Iterate through the files, read them as DataFrames, and append to the list
+# for file in balanced_file_list:
+#     file_path = os.path.join(balanced_directory, file)
+#     df = pd.read_csv(file_path)
+#     balanced_dfs.append(df)
+
+# balanced_df = pd.concat(balanced_dfs, ignore_index=True)
+# balanced_df = balanced_df.sample(frac=1).reset_index(drop=True)
+# save_batches(balanced_directory,'balanced', balanced_df)
+
+
+# unbalanced_directory = './Embeddings/Training/Unbalanced'
+# unbalanced_file_list = [file for file in os.listdir(unbalanced_directory)]
+
+# # Initialize an empty list to store DataFrames
+# unbalanced_dfs = []
+
+# # Iterate through the files, read them as DataFrames, and append to the list
+# for file in unbalanced_file_list:
+#     file_path = os.path.join(unbalanced_directory, file)
+#     df = pd.read_csv(file_path)
+#     unbalanced_dfs.append(df)
+
+# unbalanced_df = pd.concat(unbalanced_dfs, ignore_index=True)
+# unbalanced_df = unbalanced_df.sample(frac=1).reset_index(drop=True)
+# save_batches(unbalanced_directory,'unbalanced', unbalanced_df)
+
+
+
+# replace_directory = './Embeddings/Training/Replace'
+# replace_file_list = [file for file in os.listdir(replace_directory)]
+
+# # Initialize an empty list to store DataFrames
+# replace_dfs = []
+
+# # Iterate through the files, read them as DataFrames, and append to the list
+# for file in replace_file_list:
+#     file_path = os.path.join(replace_directory, file)
+#     df = pd.read_csv(file_path)
+#     replace_dfs.append(df)
+
+# replace_df = pd.concat(replace_dfs, ignore_index=True)
+# replace_df = replace_df.sample(frac=1).reset_index(drop=True)
+# save_batches(replace_directory,'replace', replace_df)
+
+
+
+strongly_balanced_replace_directory = './Embeddings/Training/Strongly_Balanced_Replace'
+strongly_balanced_replace_file_list = [file for file in os.listdir(strongly_balanced_replace_directory)]
 
 # Initialize an empty list to store DataFrames
-balanced_dfs = []
+strongly_balanced_replace_dfs = []
 
 # Iterate through the files, read them as DataFrames, and append to the list
-for file in balanced_file_list:
-    file_path = os.path.join(balanced_directory, file)
+for file in strongly_balanced_replace_file_list:
+    file_path = os.path.join(strongly_balanced_replace_directory, file)
     df = pd.read_csv(file_path)
-    balanced_dfs.append(df)
+    strongly_balanced_replace_dfs.append(df)
 
-balanced_df = pd.concat(balanced_dfs, ignore_index=True)
-balanced_df = balanced_df.sample(frac=1).reset_index(drop=True)
-save_batches(balanced_directory,'balanced', balanced_df)
+strongly_balanced_replace_df = pd.concat(strongly_balanced_replace_dfs, ignore_index=True)
+strongly_balanced_replace_df = strongly_balanced_replace_df.sample(frac=1).reset_index(drop=True)
+save_batches(strongly_balanced_replace_directory,'strongly_balanced_replace', strongly_balanced_replace_df)
 
 
-unbalanced_directory = './Embeddings/Training/Unbalanced'
-unbalanced_file_list = [file for file in os.listdir(unbalanced_directory)]
+
+strongly_balanced_directory = './Embeddings/Training/Strongly_Balanced'
+strongly_balanced_file_list = [file for file in os.listdir(strongly_balanced_directory)]
 
 # Initialize an empty list to store DataFrames
-unbalanced_dfs = []
+strongly_balanced_dfs = []
 
 # Iterate through the files, read them as DataFrames, and append to the list
-for file in unbalanced_file_list:
-    file_path = os.path.join(unbalanced_directory, file)
+for file in strongly_balanced_file_list:
+    file_path = os.path.join(strongly_balanced_directory, file)
     df = pd.read_csv(file_path)
-    unbalanced_dfs.append(df)
+    strongly_balanced_dfs.append(df)
 
-unbalanced_df = pd.concat(unbalanced_dfs, ignore_index=True)
-unbalanced_df = unbalanced_df.sample(frac=1).reset_index(drop=True)
-save_batches(unbalanced_directory,'unbalanced', unbalanced_df)
-
-
-
-replace_directory = './Embeddings/Training/Replace'
-replace_file_list = [file for file in os.listdir(replace_directory)]
-
-# Initialize an empty list to store DataFrames
-replace_dfs = []
-
-# Iterate through the files, read them as DataFrames, and append to the list
-for file in replace_file_list:
-    file_path = os.path.join(replace_directory, file)
-    df = pd.read_csv(file_path)
-    replace_dfs.append(df)
-
-replace_df = pd.concat(replace_dfs, ignore_index=True)
-replace_df = replace_df.sample(frac=1).reset_index(drop=True)
-save_batches(replace_directory,'replace', replace_df)
+strongly_balanced_df = pd.concat(strongly_balanced_dfs, ignore_index=True)
+strongly_balanced_df = strongly_balanced_df.sample(frac=1).reset_index(drop=True)
+save_batches(strongly_balanced_directory,'strongly_balanced', strongly_balanced_df)
