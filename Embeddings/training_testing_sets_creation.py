0484494810ab83a25e623dfde288afb439507df8
KieranProlog <kieran@prolog.work>
2024-03-04 17:49:39 +0100

    add model input to image embeddings

--- Embeddings/training_testing_sets_creation.py
+++ Embeddings/training_testing_sets_creation.py
@@ -10,24 +10,6 @@ import sklearn.model_selection
 import ast
 from sklearn.metrics.pairwise import cosine_similarity
 
-def calculate_distances(embedding_str):
-    start = embedding_str.find('[[')
-    end = embedding_str.find(']]')+2
-    embedding_str = embedding_str[start:end]
-    image_embedding = torch.tensor(ast.literal_eval(embedding_str))
-    image_embedding_values = np.array(image_embedding.flatten().tolist()).reshape(1, -1)
-
-    prompt_distances = []
-    # Reshape the vectors to be 2D arrays for sklearn's cosine_similarity
-    #image_embedding = image_embedding.reshape(1, -1)
-    for prompt_embedding in prompt_embeddings:
-        prompt_embedding_values = np.array(prompt_embedding.flatten().tolist()).reshape(1, -1)
-        # Calculate Cosine Similarity         
-        prompt_distances.append(cosine_similarity(image_embedding_values, prompt_embedding_values)[0,0])
-
-    model_input = np.concatenate((image_embedding_values[0], np.array(prompt_distances))).astype(np.float32)
-    return model_input.tobytes()
-
 def balance_data(max_num, unbalanced_df, country_list):
     balanced_array = []
     for index,row in country_list.iterrows():
@@ -62,8 +44,6 @@ def save_batches(path, name, dataframe):
 
 
 
-
-
 # Embeddings of Prompt
 prompt_embeddings = torch.load('./Embeddings/Prompt/prompt_image_shows_embedding.pt')
 
@@ -88,40 +68,6 @@ print('combined geoguessr databases')
 
 # Concatenate all DataFrames in the list into a single DataFrame
 combined_df = pd.concat(dfs, ignore_index=True)
-
-# Calculate distances of all aerial samples
-aerial_distances = []
-for index, row in aerial_df.iterrows():
-    aerial_distances.append(calculate_distances(row['Embedding']))
-
-aerial_df['model_input'] = aerial_distances
-model_input_aerial_df = aerial_df.drop(columns=["Embedding"])
-print('calculated aerial distances')
-
-
-# Calculate distances of all geoguessr samples
-geoguessr_distances = []
-for index, row in combined_df.iterrows():
-    geoguessr_distances.append(calculate_distances(row['Embedding']))
-    if index % 50 == 0:
-        print(f'{index} distances calculated')
-
-combined_df['model_input'] = geoguessr_distances
-model_input_combined_df = combined_df.drop(columns=["Embedding"])
-print('calculated geoguessr distances')
-
-
-# Calculate distances of all tourist samples
-tourist_distances = []
-for index, row in tourist_df.iterrows():
-    tourist_distances.append(calculate_distances(row['Embedding']))
-
-tourist_df['model_input'] = tourist_distances
-model_input_tourist_df = tourist_df.drop(columns=["Embedding"])
-print('calculated tourist distances')
-
-
-
 
 
 country_list = pd.read_csv("/home/kieran/Documents/Uni/WiSe23-24/Good_Practices_of_Machine_Learning/good_practices_ml/data_finding/country_list.csv")
