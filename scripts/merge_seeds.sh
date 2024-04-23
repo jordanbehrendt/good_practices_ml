e01e6131328b1f399c346fd82dd2c1dd7a7dbbb3
Leon Brenig <lbrenig@uni-bremen.de>
2024-04-23 10:41:12 +0200

    experiment analysis notebooks

---
+++ scripts/merge_seeds.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set the source and destination directories
+source_dir="/home/lbrenig/Documents/Uni/GPML/good_practices_ml/runs"
+destination_dir="/home/lbrenig/Documents/Uni/GPML/good_practices_ml/runs/experiment1"
+
+# Loop through all the files in the source directory
+for file in "$source_dir"/*/*/*/*/*; do
+    # Extract the necessary information from the file path 
+    # /home/lbrenig/Documents/Uni/GPML/good_practices_ml/runs/seed_3838/geo_strongly_balanced/starting_regional_loss_portion-0.0/regional_loss_decline-1.0/events.out.tfevents.1709967840.lbrenig-MS-7D09.14941.68
+    filepath=$(dirname "$file") # Set the file path to the directory of the current file in the loop
+    filename=$(basename "$file")
+    seed=$(echo "$filepath" | cut -d/ -f9 )
+    dataset_name=$(echo "$filepath" | cut -d/ -f10 )
+    loss_configuration=$(echo "$filepath" | cut -d/ -f11 )
+    # Create the destination directory if it doesn't exist
+    mkdir -p "$destination_dir/$dataset_name/$loss_configuration"
+
+    # Move the file to the destination directory with the modified filename
+    cp "$file" "$destination_dir/$dataset_name/$loss_configuration/$filename"_"$seed"
+done
---
+++ scripts/run_experiments.py
@@ -0,0 +1,11 @@
+import create_datasets_from_embddings
+import sys
+sys.path.append("./../")
+import models.model_trainer as trainer
+
+REPO_PATH = "/home/lbrenig/Documents/Uni/GPML/good_practices_ml"
+
+seeds = [4808,4947,5723,3838,5836,3947,8956,5402,1215,8980]
+for seed in seeds:
+    create_datasets_from_embddings.create_datasets_from_embddings(REPO_PATH, seed=seed)
+    trainer.create_and_train_model(REPO_PATH, seed)
