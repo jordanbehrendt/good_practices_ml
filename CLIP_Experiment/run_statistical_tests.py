a35f24799ef89438e32d21f9b3590b3bf30fc20e
KieranProlog <kieran@prolog.work>
2024-05-07 10:09:04 +0200

    finished analysis of CLIP Results

--- CLIP_Experiment/run_statistical_tests.py
+++ CLIP_Experiment/run_statistical_tests.py
@@ -12,7 +12,7 @@ import argparse
 import yaml
 import math
 
-def generate_plot(df: pd.DataFrame, plot_type: str, metric: str, experiment_name: str, experiment_dir: str) -> None:
+def generate_plot(df: pd.DataFrame, plot_type: str, metric: str, output_dir: str) -> None:
     """
     Generate and save a plot based on the given parameters.
 
@@ -20,8 +20,7 @@ def generate_plot(df: pd.DataFrame, plot_type: str, metric: str, experiment_name
         df (pd.DataFrame): DataFrame containing data for the plot.
         plot_type (str): Type of plot ('box' or 'violin').
         metric (str): Metric for the plot.
-        experiment_name (str): Name of the experiment.
-        experiment_dir (str): Directory to save the plot.
+        output_dir (str): Directory to save the plot.
     """
     if plot_type == 'box':
         sns.boxplot(data=df, showmeans=True)
@@ -30,22 +29,19 @@ def generate_plot(df: pd.DataFrame, plot_type: str, metric: str, experiment_name
 
     plt.ylabel(f"{metric} metric")
     
-    plot_filename = f"{experiment_name}_{metric}_{plot_type}plot.jpg"
-    plt.savefig(os.path.join(experiment_dir, plot_filename))
+    plot_filename = f"{plot_type}_plot.jpg"
+    plt.savefig(os.path.join(output_dir, plot_filename))
     plt.close()
 
 def compute_paired_k_fold_cross_validation_t_test(results_1:list,results_2:list,k:int,r:int):
     """
-    Compute statistical test values and save them to a CSV file.
+    Compute paired k fold cross validation t-test in accordance with the method described in https://link.springer.com/chapter/10.1007/978-3-540-24775-3_3
 
     Args:
-        accs (dict): Dictionary containing accuracy values for models.
-        metric (str): Metric for computation.
-        test_type (str): Type of statistical test ('ttest' or 'aso_test').
-        seed (int): Seed for random number generation.
-        experiment_name (str): Name of the experiment.
-        experiment_dir (str): Directory to save the test results CSV file.
-        paired_ttest (bool): Whether batches are paired.
+        results_1 (list): List of accuracies for each batch in first experiment
+        results_2 (list): List of accuracies for each batch in second experiment
+        k (int): Number of cross validation folds
+        r (int): Number of random cross-validation repetitions
     """
     if len(results_1) != len(results_2):
         raise Exception(f'The results are not the same length.')
@@ -59,116 +55,66 @@ def compute_paired_k_fold_cross_validation_t_test(results_1:list,results_2:list,
         variance += (differences[count] - mean)**2
     variance *= 1/(k*r - 1)
     t = mean / math.sqrt(variance/(k*r - 1))
-    return t
+
+    p = stats.t.sf(t, r*k-1)
+    return t,p
 
 
-# def compute_test_values(accs: dict, metric: str, test_type: str, seed: int, experiment_name: str, experiment_dir: str, paired_ttest: bool = False) -> None:
-#     """
-#     Compute statistical test values and save them to a CSV file.
-
-#     Args:
-#         accs (dict): Dictionary containing accuracy values for models.
-#         metric (str): Metric for computation.
-#         test_type (str): Type of statistical test ('ttest' or 'aso_test').
-#         seed (int): Seed for random number generation.
-#         experiment_name (str): Name of the experiment.
-#         experiment_dir (str): Directory to save the test results CSV file.
-#         paired_ttest (bool): Whether batches are paired.
-#     """
-#     model_names = list(accs.keys())
-#     accs = list(accs.values())
-#     num_datasets = len(accs)
-#     result_values = np.zeros((num_datasets, num_datasets))
-
-#     for i in range(num_datasets):
-#         for j in range(num_datasets):
-#             if test_type == 'ttest':
-#                 if paired_ttest:
-#                     result_values[i][j] = stats.ttest_rel(accs[i], accs[j]).pvalue
-#                 else:
-#                     result_values[i][j] = stats.ttest_ind(accs[i], accs[j], random_state=seed).pvalue
-#             elif test_type == 'aso_test':
-#                 result_values[i][j] = aso(accs[i], accs[j], seed=seed)
-#             else:
-#                 raise ValueError("Invalid computation type. Choose 'ttest' or 'aso_test'.")
-
-#     test_df = pd.DataFrame(result_values, columns=model_names, index=model_names)
-#     test_df.to_csv(os.path.join(experiment_dir, f'{experiment_name}_{test_type}.csv'))
-
-def run_analysis_of_dataset_and_prompts(output_dir: str,comparison_df: pd.DataFrame) -> None:
+def run_analysis_of_dataset_and_prompts(output_dir: str,comparison_df: pd.DataFrame, metric: str, paired_ttest: bool) -> None:
     """
     Save metrics DataFrame to a CSV file and calculate statistics (median, mean, std deviation) for each model.
 
     Args:
-        metrics (pd.DataFrame): DataFrame with accuracy scores for multiple models.
+        output_dir (str): Directory to save the CSV files.
+        comparison_df (str): Dataframe to be analyzed
         metric (str): Name of the metric.
-        experiment_name (str): Name of the experiment.
-        experiment_dir (str): Directory to save the CSV files.
+        paired_ttest (bool): Whether the ttest should be a paired test or not
     """
 
     # Calculate median, mean, standard deviation and p-value for each model
-    stats = {
+    analysis = {
         'Model': [],
         'Median': [],
         'Mean': [],
         'Standard Deviation': [],
-        't-test-value': []
     }
 
     for prompt in comparison_df.columns:
-        stats['Model'].append(prompt)
-        stats['Median'].append(np.median(comparison_df[prompt]))
-        stats['Mean'].append(np.mean(comparison_df[prompt]))
-        stats['Standard Deviation'].append(np.std(comparison_df[prompt]))
-        stats['t-test-value'].append(compute_paired_k_fold_cross_validation_t_test(comparison_df['default_prompt'],comparison_df['image_from_prompt'],20, 10))
+        analysis['Model'].append(prompt)
+        analysis['Median'].append(np.median(comparison_df[prompt]))
+        analysis['Mean'].append(np.mean(comparison_df[prompt]))
+        analysis['Standard Deviation'].append(np.std(comparison_df[prompt]))
 
-    # Convert the stats dictionary to a DataFrame
-    stats_df = pd.DataFrame(stats)
+    num_datasets = len(comparison_df.columns)
+
+    for i in range(num_datasets):
+        t_values = []
+        p_values = []
+        for j in range(num_datasets):
+            if i == j:
+                t_values.append(0.0)
+                p_values.append(0.0)               
+            else:
+                if paired_ttest:
+                    t,p = compute_paired_k_fold_cross_validation_t_test(comparison_df.iloc[:,i].tolist(),comparison_df.iloc[:,j].tolist(),20,10)
+                    t_values.append(t)
+                    p_values.append(p)
+                else:
+                    ttest = stats.ttest_ind(comparison_df.iloc[:,i].tolist(), comparison_df.iloc[:,j].tolist())
+                    t_values.append(ttest.statistic)
+                    p_values.append(ttest.pvalue)
+        analysis[f'{comparison_df.columns[i]}_t_values'] = t_values
+        analysis[f'{comparison_df.columns[i]}_p_values'] = p_values
+
+    # Convert the analysis dictionary to a DataFrame
+    analysis_df = pd.DataFrame(analysis)
+
+    generate_plot(comparison_df, 'box', metric, output_dir)
+    generate_plot(comparison_df, 'violin', metric, output_dir)
 
     # Save the statistics DataFrame to a CSV file
-    stats_df.to_csv(f'{output_dir}/stats.csv', index=False)
+    analysis_df.to_csv(f'{output_dir}/analysis.csv', index=False)
 
-def create_evaluation_report(accs: dict, metric: str, seed: int, paired_ttest: bool, output_dir: str, comparison_name: str) -> None:
-    """
-    Create an evaluation report containing plots and test results.
-
-    Args:
-        accs (dict): Dictionary containing experiment names and corresponding metric values.
-        metric (str): Metric for evaluation.
-        seed (int): Seed for random number generation.
-        paired_ttest (bool): Whether batches are paired.
-        output_dir (str): Output directory to save the report.
-        comparison_name (str): Custom name for comparison.
-    """
-    accs_df = pd.DataFrame(accs)
-    run_analysis_of_dataset_and_prompts(accs_df, metric, comparison_name, output_dir)
-    generate_plot(accs_df, 'box', metric, comparison_name, output_dir)
-    generate_plot(accs_df, 'violin', metric, comparison_name, output_dir)
-    # compute_test_values(accs, metric, 'ttest', seed, comparison_name, output_dir, paired_ttest)
-    # compute_test_values(accs, metric, 'aso_test', seed, comparison_name, output_dir)
-    # pdf.create_merged_pdf(output_dir, comparison_name)
-
-# def main(REPO_PATH: str, exp_dirs: List[str], exp_names: List[str], metric: str, paired_ttest: bool, comparison_name: str, seed: int) -> None:
-#     """mixed
-#     Main function to perform evaluation.
-
-#     Args:
-#         REPO_PATH (str): Path to repository.
-#         exp_dirs (List[str]): List of experiment directories.
-#         exp_names (List[str]): List of experiment names.
-#         metric (str): Metric for evaluation ('country_acc', 'region_acc' or 'mixed).
-#         paired_ttest (bool): Whether batches are paired.
-#         comparison_name (str): Custom name for comparison.
-#     """
-#     if len(exp_dirs) != len(exp_names):
-#         raise Exception(f'Length of experiment directories ({len(exp_dirs)}) is not the same as length of experiment names ({len(exp_names)}).')
-#     if metric not in ['country_acc', 'region_acc', 'mixed']:
-#         raise Exception(f'The metric {metric} is not known. Instead use either country_acc, region_acc or mixed')
-#     output_dir = os.path.join(REPO_PATH, 'CLIP_Experiment/analysis/', comparison_name)
-#     if not os.path.exists(output_dir):
-#         os.makedirs(output_dir)
-#     metric_dict = geo_metrics.calculate_experiment_metric(REPO_PATH, exp_dirs, exp_names, metric)
-#     create_evaluation_report(metric_dict, metric, seed, paired_ttest, output_dir, comparison_name)
 
 def run_analysis_of_experiments(REPO_PATH, metric):
     """
@@ -176,10 +122,13 @@ def run_analysis_of_experiments(REPO_PATH, metric):
 
     Args:
         REPO_PATH (str): path to the repo folder.
+        metric (str): name of metric used
     """
     seeds = [4808,4947,5723,3838,5836,3947,8956,5402,1215,8980]
 
     datasets = ['geoguessr', 'aerial', 'tourist']
+    default_df = pd.DataFrame()
+    image_from_df = pd.DataFrame()
     for dataset in datasets:
         default_experiment = pd.read_csv(f"{REPO_PATH}/CLIP_Experiment/metrics/{metric}/default_prompt/{dataset}.csv")
         image_from_experiment = pd.read_csv(f"{REPO_PATH}/CLIP_Experiment/metrics/{metric}/image_from_prompt/{dataset}.csv")
@@ -189,23 +138,21 @@ def run_analysis_of_experiments(REPO_PATH, metric):
             default_list += list(default_experiment[f'{seed}'])
             image_from_list += list(image_from_experiment[f'{seed}'])
         comparison_df = pd.DataFrame({'default_prompt': default_list, 'image_from_prompt': image_from_list})
+        default_df[f'{dataset}'] = default_list
+        image_from_df[f'{dataset}'] = image_from_list
         output_dir = f'{REPO_PATH}/CLIP_Experiment/statistical_tests/{metric}/{dataset}/'
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-        run_analysis_of_dataset_and_prompts(output_dir,comparison_df)
-
-    # experiment_dirs = [
-    #     f"{REPO_PATH}/CLIP_Experiment/metrics/{metric}/default_prompt/geoguessr",
-    #     f"{REPO_PATH}/CLIP_Experiment/metrics/{metric}/default_prompt/tourist",
-    #     f"{REPO_PATH}/CLIP_Experiment/metrics/{metric}/default_prompt/aerial",
-    #     f"{REPO_PATH}/CLIP_Experiment/metrics/{metric}/image_from_prompt/geoguessr",
-    #     f"{REPO_PATH}/CLIP_Experiment/metrics/{metric}/image_from_prompt/tourist",
-    #     f"{REPO_PATH}/CLIP_Experiment/metrics/{metric}/image_from_prompt/aerial",
-    # ]
-
-    # # Run comparison of datasets for image_from_prompt
-    # main(REPO_PATH, [experiment_dirs[3], experiment_dirs[4], experiment_dirs[5]], ['geoguessr', 'tourist', 'aerial'], 'mixed', False, 'datasets_image_prompt_mixed', seed)
-    # main(REPO_PATH, [experiment_dirs[0], experiment_dirs[1], experiment_dirs[2]], ['geoguessr', 'tourist', 'aerial'], 'mixed', False, 'datasets_default_prompt_mixed', seed)
+        run_analysis_of_dataset_and_prompts(output_dir,comparison_df, metric, True)
+    
+    default_output_dir = f'{REPO_PATH}/CLIP_Experiment/statistical_tests/{metric}/default_prompt/'
+    image_from_output_dir = f'{REPO_PATH}/CLIP_Experiment/statistical_tests/{metric}/image_from_prompt/'
+    if not os.path.exists(default_output_dir):
+        os.makedirs(default_output_dir)
+    if not os.path.exists(image_from_output_dir):
+        os.makedirs(image_from_output_dir)
+    run_analysis_of_dataset_and_prompts(default_output_dir,default_df, metric, False)
+    run_analysis_of_dataset_and_prompts(image_from_output_dir,image_from_df, metric, False)
 
 if __name__ == "__main__":
     """
