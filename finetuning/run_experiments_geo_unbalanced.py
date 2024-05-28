e9849a7f76acbed89b5e3b32fd37ed028179ec00
jordanbehrendt <jobe@uni-bremen.de>
2024-05-28 11:03:12 +0200

    UPDATE adjust training

--- finetuning/run_experiments_geo_unbalanced.py
+++ finetuning/run_experiments_geo_unbalanced.py
@@ -4,8 +4,10 @@ sys.path.append("./../")
 import finetuning.model.model_trainer as trainer
 
 REPO_PATH = "/share/temp/bjordan/good_practices_in_machine_learning/good_practices_ml/"
-
+training_datasets = [
+        'geo_unbalanced.csv'
+    ]
 #create_datasets_from_embddings.create_datasets_from_embddings(REPO_PATH, seed=1234)
 seeds = [4808,4947,5723,3838,5836,3947,8956,5402,1215,8980]
 for seed in seeds:
-    trainer.create_and_train_model(REPO_PATH, seed)
+    trainer.create_and_train_model(REPO_PATH, seed, training_datasets)
