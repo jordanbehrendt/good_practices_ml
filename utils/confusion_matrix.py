4e324fbaa78c49d9c300096b290f03b498322009
KieranProlog <kieran@prolog.work>
2024-06-04 13:37:15 +0200

    add some comments to the confusion_matrix file

--- utils/confusion_matrix.py
+++ utils/confusion_matrix.py
@@ -21,6 +21,7 @@ def create_and_save_confusion_matrices(REPO_PATH, SAVE_FIGURES_PATH, true_countr
         None
     """
 
+    # Load country list and regional ordering index
     country_list = pd.read_csv(f'{REPO_PATH}/utils/country_list/country_list_region_and_continent.csv')
     regional_ordering_index = [8, 11, 144, 3, 4, 12, 16, 26, 28, 44, 46, 51, 52, 66, 74, 83, 95, 101, 105, 109, 121, 128, 153, 180, 191, 201, 202, 32, 43, 77, 81, 134, 140, 146, 179, 99, 106, 185, 187, 198, 58, 98, 122, 131, 133, 136, 159, 163, 166, 177, 178, 193, 195, 209, 210, 41, 80, 97, 102, 103, 126, 127, 192, 20, 31, 48, 84, 119, 152, 160, 162, 173, 194, 60, 137, 149, 165, 204, 78, 156, 7, 34, 35, 40, 64, 53, 56, 116, 117, 167, 188, 23, 33, 72, 196, 13, 50, 55, 59, 62, 65, 69,
                                     86, 88, 92, 94, 113, 115, 142, 168, 172, 38, 148, 189, 205, 9, 25, 27, 39, 42, 54, 61, 68, 76, 79, 147, 157, 197, 200, 24, 85, 100, 107, 125, 135, 150, 169, 184, 186, 203, 30, 138, 182, 208, 2, 17, 29, 89, 91, 111, 132, 143, 151, 0, 5, 15, 57, 71, 75, 82, 93, 120, 123, 130, 155, 161, 171, 175, 199, 206, 19, 22, 37, 45, 70, 73, 112, 124, 129, 139, 170, 174, 176, 183, 1, 6, 14, 21, 47, 67, 87, 90, 96, 104, 108, 145, 154, 158, 164, 181, 190, 207, 10, 18, 36, 49, 63, 110, 114, 118, 141]
@@ -29,16 +30,14 @@ def create_and_save_confusion_matrices(REPO_PATH, SAVE_FIGURES_PATH, true_countr
     classes = country_list['Country']
     np_classes = np.array(classes)
 
-    # Build country confusion matrix
+    # Build country confusion matrices
     if normalize:
         cf_matrix = confusion_matrix(true_countries, predicted_countries, labels=range(0, 211), normalize='true')
     else:
         cf_matrix = confusion_matrix(true_countries, predicted_countries, labels=range(0, 211))
     ordered_index = np.argsort(-cf_matrix.diagonal())
     ordered_matrix = cf_matrix[ordered_index][:, ordered_index]
-
     regionally_ordered_matrix = cf_matrix[regional_ordering_index][:,regional_ordering_index]
-
     ordered_classes = np_classes[ordered_index]
     regionally_ordered_classes = np_classes[regional_ordering_index]
 
@@ -48,15 +47,15 @@ def create_and_save_confusion_matrices(REPO_PATH, SAVE_FIGURES_PATH, true_countr
     regionally_ordered_df_cm = pd.DataFrame(
         regionally_ordered_matrix, index=regionally_ordered_classes, columns=regionally_ordered_classes)
 
+    #Create region labels
     np_regions = np.sort(np.array(list(set(country_list['Intermediate Region Name']))))
-
-    # Build region confusion matrix
     true_regions = []
     predicted_regions = []
     for i in range(0, len(true_countries)):
         true_regions.append(ast.literal_eval(country_list.iloc[true_countries[i]]["One Hot Region"]).index(1))
         predicted_regions.append(ast.literal_eval(country_list.iloc[predicted_countries[i]]["One Hot Region"]).index(1))
 
+    # Build region confusion matrices
     if normalize:
         regions_cf_matrix = confusion_matrix(true_regions, predicted_regions, labels=range(0, len(np_regions)), normalize='true')
     else:
@@ -80,6 +79,7 @@ def create_and_save_confusion_matrices(REPO_PATH, SAVE_FIGURES_PATH, true_countr
     sns.set(font_scale=8)
     ax_1 = sns.heatmap(df_cm, cmap=sns.cubehelix_palette(as_cmap=True),xticklabels = 1,yticklabels=1)
     ax_1.tick_params(axis='both', labelsize=15)
+    ax_1.set(xlabel=None, ylabel=None)
     if normalize:
         ax_1.figure.savefig(f'{SAVE_FIGURES_PATH}/normalized/simple_confusion_matrix.png')
     else:
@@ -87,6 +87,7 @@ def create_and_save_confusion_matrices(REPO_PATH, SAVE_FIGURES_PATH, true_countr
     fig_2, ax_2 = plt.subplots(figsize=(120, 90))
     ax_2 = sns.heatmap(ordered_df_cm, cmap=sns.cubehelix_palette(as_cmap=True),xticklabels=1,yticklabels=1)
     ax_2.tick_params(axis='both', labelsize=15)
+    ax_2.set(xlabel=None, ylabel=None)
     if normalize:
         ax_2.figure.savefig(f'{SAVE_FIGURES_PATH}/normalized/ordered_confusion_matrix.png')
     else:
@@ -94,6 +95,7 @@ def create_and_save_confusion_matrices(REPO_PATH, SAVE_FIGURES_PATH, true_countr
     fig_3, ax_3 = plt.subplots(figsize=(120, 90))
     ax_3 = sns.heatmap(regionally_ordered_df_cm, cmap=sns.cubehelix_palette(as_cmap=True),xticklabels=1,yticklabels=1)
     ax_3.tick_params(axis='both', labelsize=15)
+    ax_3.set(xlabel=None, ylabel=None)
     if normalize:
         ax_3.figure.savefig(f'{SAVE_FIGURES_PATH}/normalized/regionally_ordered_confusion_matrix.png')
     else:
@@ -101,6 +103,7 @@ def create_and_save_confusion_matrices(REPO_PATH, SAVE_FIGURES_PATH, true_countr
     fig_4, ax_4 = plt.subplots(figsize=(120, 90))
     ax_4 = sns.heatmap(regions_df_cm, cmap=sns.cubehelix_palette(as_cmap=True),xticklabels = 1,yticklabels=1)
     ax_4.tick_params(axis='both', labelsize=50)
+    ax_4.set(xlabel=None, ylabel=None)
     if normalize:
         ax_4.figure.savefig(f'{SAVE_FIGURES_PATH}/normalized/regions_confusion_matrix.png')
     else:
@@ -108,6 +111,7 @@ def create_and_save_confusion_matrices(REPO_PATH, SAVE_FIGURES_PATH, true_countr
     fig_5, ax_5 = plt.subplots(figsize=(120, 90))
     ax_5 = sns.heatmap(regions_ordered_df_cm, cmap=sns.cubehelix_palette(as_cmap=True),xticklabels = 1,yticklabels=1)
     ax_5.tick_params(axis='both', labelsize=50)
+    ax_5.set(xlabel=None, ylabel=None)
     if normalize:
         ax_5.figure.savefig(f'{SAVE_FIGURES_PATH}/normalized/regions_ordered_confusion_matrix.png')
     else:
